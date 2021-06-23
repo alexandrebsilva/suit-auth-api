e16967665441232fb943d76fca7b4d692e6604a4
Alexandre Borges <alexandre.bsilva55@gmail.com>
2021-06-23 01:42:58 -0300

    changing port

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,6 @@ const app = createExpressServer({
 });
 
 // run express application on port 3000
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
